6b6b359ee8190a6585132d495f661ab24c21d7b8
Garrett Neuenkirchen <garrettn@gmail.com>
2021-01-17 20:28:45 -0600

    forgot to check this in before.

--- chapter5.js
+++ chapter5.js
@@ -52,3 +52,22 @@ function map(array, transform) {
 }
 let rtlScripts = SCRIPTS.filter(s => s.direction == "rtl");
 console.log(map(rtlScripts, s => s.name));
+
+//reduce
+function reduce(array, combine, start) {
+    let current = start;
+    for (let element of array) {
+        current = combine(current, element);
+    }
+    return current;
+}
+console.log(reduce([1,2,3,4], (a,b) => a+b, 0));
+
+function characterCount(script) {
+    return script.ranges.reduce((count, [from, to]) => {
+        return count + (to - from);
+    }, 0);
+}
+console.log(SCRIPTS.reduce((a, b) => {
+    return characterCount(a) < characterCount(b) ? b:a;
+}))
